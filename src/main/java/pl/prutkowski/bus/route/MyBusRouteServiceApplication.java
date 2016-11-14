8b4ec4fcd37437e33ede8ea7e7e577b796c59a18
pr-ogrammer <rutkowski1988@gmail.com>
2016-11-14 18:05:46 +0100

    Spring Boot initialized app

---
+++ src/main/java/pl/prutkowski/bus/route/MyBusRouteServiceApplication.java
@@ -0,0 +1,12 @@
+package pl.prutkowski.bus.route;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyBusRouteServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyBusRouteServiceApplication.class, args);
+	}
+}
